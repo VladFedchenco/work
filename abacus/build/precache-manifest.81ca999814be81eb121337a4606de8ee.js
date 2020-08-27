84ee4bfd00caf5e448d03a27d0e760742b65e59b
 Vladyslav <fedchenco@gmail.com>
2020-08-28 01:11:54 +0300

    Abacus site animation v1

---
+++ abacus/build/precache-manifest.81ca999814be81eb121337a4606de8ee.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b140c5894a286ec8f88bc127a9e7bec5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b32c2fa420594e4c6356",
+    "url": "/static/css/2.70e35f80.chunk.css"
+  },
+  {
+    "revision": "b21ac60a0659c5f6f034",
+    "url": "/static/css/main.536efd68.chunk.css"
+  },
+  {
+    "revision": "b32c2fa420594e4c6356",
+    "url": "/static/js/2.10f4c243.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.10f4c243.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b21ac60a0659c5f6f034",
+    "url": "/static/js/main.8be51c89.chunk.js"
+  },
+  {
+    "revision": "74bd1083d9e6e05c766e",
+    "url": "/static/js/runtime-main.cc4fb634.js"
+  },
+  {
+    "revision": "a312c9d3b2c59413f681fe84ed45a027",
+    "url": "/static/media/abacus_animation.a312c9d3.svg"
+  },
+  {
+    "revision": "c98153c93fb95696ada4682bd758c599",
+    "url": "/static/media/lng-tanker.c98153c9.svg"
+  }
+]);
