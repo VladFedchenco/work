d7a30f681926cc83c55aff6118e92583ec9aac2b
 Vladyslav <fedchenco@gmail.com>
2020-10-29 18:41:35 +0200

    AB test full pages

--- cary/html/js/main.js
+++ cary/html/js/main.js
@@ -6,24 +6,21 @@ $("#click_left").onclick = function() {
   clearTimeout(tmt);
   $("#smoke").setAttribute("class", "click_point_01");
   $("#section_circles").setAttribute("class", "invis");
-  $("header").setAttribute("class", "invis");
-  $("#rating_text").innerHTML = "I'm on Team Starbucks!";
-  $("#rating_logo").setAttribute("src", "imgs/sb.svg");
   tmt = setTimeout(function(){
     $("#smoke").setAttribute("class", "");
-    $("#rating").setAttribute("class", "active");
+    $("#rate_sb").classList.add("active");
   }, 2000);
+}
+$("#click_middle").onclick = function() {
+  $("#drop_down").classList.add("active");
 }
 $("#click_right").onclick = function() {
   clearTimeout(tmt);
   $("#smoke").setAttribute("class", "click_point_03");
   $("#section_circles").setAttribute("class", "invis");
-  $("header").setAttribute("class", "invis");
-  $("#rating_text").innerHTML = "I'm on Team Tim Hortons!";
-  $("#rating_logo").setAttribute("src", "imgs/th.svg");
   tmt = setTimeout(function(){
-    $("#smoke").setAttribute("class", "")
-    $("#rating").setAttribute("class", "active");
+    $("#smoke").setAttribute("class", "");
+    $("#rate_th").classList.add("active");
   }, 2000);
 }
 
@@ -33,6 +30,9 @@ $("#bttn_info").onclick = function() {
 $("#helper_close").onclick = function() {
   $("#helper").classList.remove("active");
 }
+$("#drop_down_close").onclick = function() {
+  $("#drop_down").classList.remove("active");
+}
 
 $("#bttn_more").onclick = function() {
   $("#more_details").classList.add("active");
@@ -41,62 +41,76 @@ $("#more_details_close").onclick = function() {
   $("#more_details").classList.remove("active");
 }
 
+function sb_thanks() {
+  $("#rate_sb .thanks").classList.add("active");
+}
+
+$("#sb_star1").onclick = function() {sb_thanks()};
+$("#sb_star2").onclick = function() {sb_thanks()};
+$("#sb_star3").onclick = function() {sb_thanks()};
+$("#sb_star4").onclick = function() {sb_thanks()};
+$("#sb_star5").onclick = function() {sb_thanks()};
+
+function th_thanks() {
+  $("#rate_th .thanks").classList.add("active");
+}
+
+$("#th_star1").onclick = function() {th_thanks()};
+$("#th_star2").onclick = function() {th_thanks()};
+$("#th_star3").onclick = function() {th_thanks()};
+$("#th_star4").onclick = function() {th_thanks()};
+$("#th_star5").onclick = function() {th_thanks()};
+
+function show_result() {
+  $("#result").classList.add("active");
+}
+
+$("#bttn_sb_ok").onclick = function() {
+  $("#rate_sb").classList.remove("active");
+  show_result()
+};
+$("#bttn_th_ok").onclick = function() {
+  $("#rate_th").classList.remove("active");
+  show_result()
+};
+
+$("#result_tell").onclick = function() {
+  $("#why").classList.add("active");
+};
+$("#why_close").onclick = function() {
+  $("#why").classList.remove("active");
+}
+$("#bttn_cancel").onclick = function() {
+  $("#why").classList.remove("active");
+}
+
+$("#form_skip").onclick = function() {
+  window.location.reload(false); 
+}
+
+$("#result_skip").onclick = function() {
+  $("#result").classList.remove("active");
+  $("#sign_up").classList.add("active");
+}
+
+let form_block = $("#tell_why");
+form_block.addEventListener("submit", function(event) {
+  event.preventDefault();
+  $("#result").classList.remove("active");
+  $("#why").classList.remove("active");
+  $("#sign_up").classList.add("active");
+});
+
+$("#form_submit").onclick = function() {
+  $("#sign_up").classList.remove("active");
+  $("#sign_up_thanks").classList.add("active");
+}
+
+$("#final_thanks").onclick = function() {
+  window.location.reload(false); 
+}
+
 window.addEventListener('load', (event) => {
   $("#initial_screen").classList.add("active");
   setTimeout(function(){ $("#initial_screen").classList.remove("active"); }, 8000);
-});
-
-$("#sb_rate1").addEventListener("mouseover", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ffb400");
-});
-$("#sb_rate1").addEventListener("mouseout", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ccc");
-});
-
-$("#sb_rate2").addEventListener("mouseover", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ffb400");
-});
-$("#sb_rate2").addEventListener("mouseout", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ccc");
-});
-
-$("#sb_rate3").addEventListener("mouseover", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate3 svg polygon").setAttribute("fill", "#ffb400");
-});
-$("#sb_rate3").addEventListener("mouseout", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate3 svg polygon").setAttribute("fill", "#ccc");
-});
-
-$("#sb_rate4").addEventListener("mouseover", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate3 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate4 svg polygon").setAttribute("fill", "#ffb400");
-});
-$("#sb_rate4").addEventListener("mouseout", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate3 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate4 svg polygon").setAttribute("fill", "#ccc");
-});
-
-$("#sb_rate5").addEventListener("mouseover", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate3 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate4 svg polygon").setAttribute("fill", "#ffb400");
-  $("#sb_rate5 svg polygon").setAttribute("fill", "#ffb400");
-});
-$("#sb_rate5").addEventListener("mouseout", function(){
-  $("#sb_rate1 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate2 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate3 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate4 svg polygon").setAttribute("fill", "#ccc");
-  $("#sb_rate5 svg polygon").setAttribute("fill", "#ccc");
 });
