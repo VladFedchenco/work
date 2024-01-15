33e88e023ee3f0c57e60707af50100cb4619c847
VladFedchenco <64959579+VladFedchenco@users.noreply.github.com>
2024-01-15 18:13:06 +0200

    topoil wiper short version

---
+++ wsp/topoil/html_05/js/main.js
@@ -0,0 +1,38 @@
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
+
+request.onload = function() {
+  const prizeData = request.response;
+    $("#entries").innerHTML = prizeData.entries;
+    $("#prize").setAttribute("href", prizeData.prize_id);
+}
+
+function $(sel) {
+  return document.querySelector(sel);
+}
+
+function play_sound() {
+  $("#sound").play();
+  $("#sound").loop=false;
+}
+
+function gstart() {
+  play_sound();
+  $("#animated_parts").classList.add("animated");
+  $("#start").classList.add("invis");
+  $("#animation").style.cursor = "default";
+  $("#animation").style.pointerEvents = "none";
+  setTimeout(function(){
+    $("#content").classList.remove("invis");
+  }, 3500);
+}
+
+$("#start").addEventListener("click", function(){
+  gstart();
+}, false);
+
+$("#animation").addEventListener("click", function(){
+  gstart();
+}, false);
