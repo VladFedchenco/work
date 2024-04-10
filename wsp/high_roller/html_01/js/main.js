4908062bfb9895fb76dd3c2fbe681f2b8bae70de
VladFedchenco <64959579+VladFedchenco@users.noreply.github.com>
2024-04-10 17:05:34 +0300

    high roller v01

---
+++ wsp/high_roller/html_01/js/main.js
@@ -0,0 +1,127 @@
+// let play_game = true;
+// let prize_ID;
+// let requestURL;
+
+// requestURL = 'js/data.json';
+// let request = new XMLHttpRequest();
+// request.open('GET', requestURL);
+// request.responseType = 'json';
+// request.send();
+
+// request.onload = function() {
+//   const prizeData = request.response;
+//   prize_ID = prizeData.prize_ID;
+//   $("#select_prize1").setAttribute("src", prizeData.img1);
+//   $("#prize_ttl1").innerHTML =  prizeData.title1;
+//   $("#prize_sub1").innerHTML =  prizeData.subtitle1;
+//   $("#select_prize2").setAttribute("src", prizeData.img2);
+//   $("#prize_ttl2").innerHTML =  prizeData.title2;
+//   $("#prize_sub2").innerHTML =  prizeData.subtitle2;
+// }
+
+// $("#bttn_prize1").addEventListener("click", function(){
+//   prize_selection(1);
+// }, false);
+
+// $("#bttn_prize2").addEventListener("click", function(){
+//   prize_selection(2);
+// }, false);
+
+// $("#game").addEventListener("click", function(){
+//   if(play_game && prize_ID >= 3010 && prize_ID <= 3034) {
+//     gstart();
+//     $("#game").classList.add("played");
+//     play_game = false;
+//   }
+// }, false);
+
+function $(sel) {
+  return document.querySelector(sel);
+}
+
+function play_sound() {
+  $("#sound").play();
+  $("#sound").loop = false;
+}
+
+function play_sound2() {
+  $("#sound2").play();
+  $("#sound2").loop = false;
+}
+
+// function gstart() {
+//   play_sound();
+//   $("#handle1").classList.add("invis");
+//   $("#handle2").classList.remove("invis");
+//   $("#initial").classList.add("initial");
+//   $("#prize1").classList.add("a" + prize_ID + "_1");
+//   $("#prize2").classList.add("a" + prize_ID + "_2");
+//   $("#info_before").classList.add("transp");
+//   setTimeout(function(){
+//     $("#handle1").classList.remove("invis");
+//     $("#handle2").classList.add("invis");
+//     $("#info_before").classList.add("invis");
+//     $("#info_after").classList.remove("invis");
+//   }, 400);
+//   setTimeout(function(){
+//     $("#game").classList.add("transparent");
+//     $("#info").classList.remove("transp");
+//     $("#info_after").classList.remove("transp");
+//     $("#prizes").classList.add("ready");
+//   }, 8000);
+// }
+
+// function prize_selection(id) {
+//   $("#bttn_prize1").classList.add("inactive")
+//   $("#bttn_prize2").classList.add("inactive")
+//   $("#loader").classList.remove("invis");
+//   $("body").style.position = "fixed";
+//   setTimeout(function(){
+//     $("#loader").classList.add("invis");
+//     $("body").removeAttribute("style");
+//     $("#info h2").innerHTML =  "CONGRATULATIONS!";
+//     $("#info h3").innerHTML =  "Your prize:";
+//     $("#prizes").classList.add("invis");
+//     $("#prizes").classList.remove("ready");
+//     $("#prizes").classList.add("splash");
+//     id == 1 ? $("#bttn_prize2").classList.add("invis") : $("#bttn_prize1").classList.add("invis");
+//     $("#info_after").classList.add("invis");
+//     setTimeout(function(){
+//       $("#prizes").classList.remove("invis");
+//       setTimeout(function(){
+//         $("#prizes").classList.add("ready");
+//         $("#vendors").classList.add("visible");
+//       }, 50);
+//     }, 50);
+//   }, 2000);
+// }
+
+let prize_ID = 18019;
+let start = true;
+
+$("#pump").addEventListener("click", function(){
+  $("#hand").classList.add("invis");
+  $("#spin_bttn").classList.add("invis");
+  $("#coins").classList.add("invis");
+  $("#prize_01").classList.add("a" + prize_ID + "_1");
+  $("#prize_02").classList.add("a" + prize_ID + "_2");
+  $("#prize_03").classList.add("a" + prize_ID + "_3");
+  if(start) {
+    start = false;
+    play_sound();
+    $("#handle_01").classList.add("invis");
+    $("#handle_02").classList.remove("invis");
+    $("#pump").classList.add("clicked");
+    setTimeout(function(){
+      $("#handle_01").classList.remove("invis");
+      $("#handle_02").classList.add("invis");
+    }, 400);
+    setTimeout(function(){
+      play_sound2();
+      $("#main_popup").classList.remove("invis");
+      $("#message_frame").classList.add("anim");
+      $("#message_options").classList.add("anim");
+    }, 6500);
+  }
+}, false);
+
