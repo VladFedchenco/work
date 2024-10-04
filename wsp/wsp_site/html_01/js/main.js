03f428945d8b9ebcc8fb33afaff6adcd92ef6a57
VladFedchenco <64959579+VladFedchenco@users.noreply.github.com>
2024-10-04 22:49:18 +0300

    all pages

--- wsp/wsp_site/html_01/js/main.js
+++ wsp/wsp_site/html_01/js/main.js
@@ -1,17 +1,21 @@
-function $(sel) {
-  return document.querySelector(sel);
-}
+let big_logo = $("#logo_big");
 
-window.addEventListener("scroll", function() {
-  if (window.scrollY >= 100) {
-    $("#logo").classList.remove("hidden");
-    $("#logo_big").classList.add("hidden");
-  } else {
-    $("#logo").classList.add("hidden");
-    $("#logo_big").classList.remove("hidden");
-  }
-});
+if (big_logo) {
+  window.addEventListener("scroll", function() {
+      if (window.scrollY >= 100) {
+        $("#logo").classList.remove("hidden");
+        $("#logo_big").classList.add("hidden");
+      } else {
+        $("#logo").classList.add("hidden");
+        $("#logo_big").classList.remove("hidden");
+      }
+  });
+}
 
 $("#mobile_menu").addEventListener("click", function() {
   $("body").classList.toggle("menu_active");
 });
+
+function $(sel) {
+  return document.querySelector(sel);
+}
