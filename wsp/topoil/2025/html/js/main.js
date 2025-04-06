e77cadf67dda4dd4d772fadaf2e0361297ba653b
VladFedchenco <64959579+VladFedchenco@users.noreply.github.com>
2025-04-06 17:42:59 +0300

    top oil 2025 update 1

--- wsp/topoil/2025/html/js/main.js
+++ wsp/topoil/2025/html/js/main.js
@@ -1,13 +1,12 @@
-// let request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
 
-// request.onload = function() {
-//   const prizeData = request.response;
-//     $("#entries").innerHTML = prizeData.entries;
-//     $("#prize").setAttribute("href", prizeData.prize_id);
-// }
+request.onload = function() {
+  const prizeData = request.response;
+    $("#ballots h2").innerHTML = prizeData.ballots;
+}
 
 function $(sel) {
   return document.querySelector(sel);
@@ -15,13 +14,12 @@ function $(sel) {
 
 function play_sound() {
   $("#sound").play();
-  $("#sound").loop=false;
+  $("#sound").loop = false;
 }
 
 function gstart() {
   // play_sound();
-  $("#animation").style.cursor = "default";
-  $("#animation").style.pointerEvents = "none";
+  $("#animation").classList.add("inactive");
   $("#hand").classList.add("invis");
   $("#tap").classList.add("invis");
   $("#fuel").classList.add("invis");
@@ -30,22 +28,22 @@ function gstart() {
   $("#digits").classList.add("move");
   $("#winner").classList.add("win9");
   setTimeout(function(){
+    $("#confetti").setAttribute("href", "imgs/confetti.svg");
+  }, 3800);
+  setTimeout(function(){
     $("#station").classList.add("invis");
-  }, 5000);
+  }, 4800);
   setTimeout(function(){
     $("#receipt").classList.add("active");
-  }, 5300);
+  }, 5100);
   setTimeout(function(){
     $("#win_circle").classList.add("active");
     $("#ballots").classList.add("active");
-  }, 5800);
+  }, 5600);
   setTimeout(function(){
     $("#info").classList.add("active");
     $("#code").classList.add("active");
-  }, 6300);
-  setTimeout(function(){
-    $("#confetti").setAttribute("href", "imgs/confetti.svg");
-  }, 7000);
+  }, 6100);
 }
 
 $("#animation").addEventListener("click", function(){
