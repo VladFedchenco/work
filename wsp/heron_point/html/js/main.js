9790c87d1706996892999444b5155e946ba1d22c
VladFedchenco <64959579+VladFedchenco@users.noreply.github.com>
2025-05-27 20:53:54 +0300

    heron point v2

--- wsp/heron_point/html/js/main.js
+++ wsp/heron_point/html/js/main.js
@@ -1,19 +1,34 @@
-// let game_type = 1;
+let gametype, color1, color2, color3, product1, product2, product3, descriprion1, descriprion2, descriprion3;
 
-// let request = new XMLHttpRequest();
-// request.open('GET', requestURL);
-// request.responseType = 'json';
-// request.send();
+let request = new XMLHttpRequest();
+request.open('GET', requestURL);
+request.responseType = 'json';
+request.send();
 
-// request.onload = function() {
-//   const prizeData = request.response;
-//     $("#bllt").innerHTML = prizeData.ballots;
-//     game_type = prizeData.game_type;
-//     win = "win" + prizeData.ballots;
-//     gstart();
-// }
+request.onload = function() {
+  const prizeData = request.response;
+    gametype = prizeData.gametype;
+    $("#can1 img").setAttribute("src", prizeData.can1);
+    $("#can2 img").setAttribute("src", prizeData.can2);
+    $("#can3 img").setAttribute("src", prizeData.can3);
+    $("#open1").setAttribute("src", prizeData.can1_open);
+    $("#open2").setAttribute("src", prizeData.can2_open);
+    $("#open3").setAttribute("src", prizeData.can3_open);
+    color1 = prizeData.can1_color;
+    color2 = prizeData.can2_color;
+    color3 = prizeData.can3_color;
+    product1 = prizeData.can1_product;
+    product2 = prizeData.can2_product;
+    product3 = prizeData.can3_product;
+    descriprion1 = prizeData.can1_descriprion;
+    descriprion2 = prizeData.can2_descriprion;
+    descriprion3 = prizeData.can3_descriprion;
+}
 
 $("#can1").addEventListener("click", () => {
+  $("#banner_win").style.background = color1;
+  $("#banner_win h2").innerHTML = product1;
+  $("#banner_win h3").innerHTML = descriprion1;
   hide_cans();
   setTimeout(function(){
     $("#open1").classList.remove("invis");
@@ -24,6 +39,9 @@ $("#can1").addEventListener("click", () => {
 }, false);
 
 $("#can2").addEventListener("click", () => {
+  $("#banner_win").style.background = color2;
+  $("#banner_win h2").innerHTML = product2;
+  $("#banner_win h3").innerHTML = descriprion2;
   hide_cans();
   setTimeout(function(){
     $("#open2").classList.remove("invis");
@@ -34,6 +52,9 @@ $("#can2").addEventListener("click", () => {
 }, false);
 
 $("#can3").addEventListener("click", () => {
+  $("#banner_win").style.background = color3;
+  $("#banner_win h2").innerHTML = product3;
+  $("#banner_win h3").innerHTML = descriprion3;
   hide_cans();
   setTimeout(function(){
     $("#open3").classList.remove("invis");
@@ -45,7 +66,13 @@ $("#can3").addEventListener("click", () => {
 
 const randomNumber = Math.floor(Math.random() * 1000) + 1;
 
+function play_sound() {
+  $("#sound").play();
+  $("#sound").loop=false;
+}
+
 function hide_cans() {
+  play_sound();
   if (gametype == 2) {
     $("#prize").setAttribute("src", "imgs/tumbler.png");
   }
@@ -86,11 +113,4 @@ function hide_cans() {
 
 function $(sel) {
   return document.querySelector(sel);
-}
-
-function gstart() {
-
-  setTimeout(function(){
-    
-  }, 500);
 }
